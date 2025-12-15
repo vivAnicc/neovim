b3292ead8eda070b2046e13efdd489df4e98a9fb
nick <nicc.gemm@gmail.com>
2025-12-15 22:59:01 +0100

    First commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    utils = {
+      url = "github:vivAnicc/nix-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixvim, utils, ... }: utils.lib.mkFlake (system: let
+      nixvim' = nixvim.legacyPackages.${system};
+      nixvimModule = {
+        inherit system;
+        module = import ./config.nix;
+        extraSpecialArgs = {
+        };
+      };
+      nvim = nixvim'.makeNixvimWithModule nixvimModule;
+    in {
+      packages.${system} = {
+        inherit nvim;
+        default = nvim;
+      };
+    });
+}
